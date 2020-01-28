9dc6ccdc9d5939eb0caa5a585e55766cc04b8775
Silas <56266659+SilasDerProfi@users.noreply.github.com>
2020-01-28 19:02:12 +0100

    Create MultiplicationTable.java

---
+++ control/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package control;
+
+/**
+ * MultiplicationTable in one Line
+ */
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        for (int i = 10; i <= 109; System.out.printf("%6s", ++i % 10 == 0 ? i - 10 + "\n" : "\b" + i % 10 * (i / 10)));
+    }
+}
